9412551a4de77dd6163c620d51955fa3e6c45cee
Graham Gower <graham.gower@gmail.com>
2021-07-14 07:57:28 +0200

    fix for github's broken dependency tracking

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 from setuptools import setup
 
-setup(use_scm_version={"write_to": "demes/_version.py"})
+setup(
+    # Set the name so that github correctly tracks reverse dependencies.
+    # https://github.com/popsim-consortium/demes-python/network/dependents
+    name="demes",
+    use_scm_version={"write_to": "demes/_version.py"},
+)
